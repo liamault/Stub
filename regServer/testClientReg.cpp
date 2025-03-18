7a61c68051d2776accbbdd8ec4a2f05313eade7a
SyedMoiz1 <95774468+SyedMoiz1@users.noreply.github.com>
2025-03-18 12:28:38 -0400

    reg test client

--- regServer/testClientReg.cpp
+++ regServer/testClientReg.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <cstring>
+#include <netdb.h>  
+#include <sys/types.h> 
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include "regulatory_to_broker.pb.h"
 #include "common.pb.h"
 #include <google/protobuf/descriptor.h>
+#include "../naming/svcDirClient.hpp"
 
 using namespace std;
 
@@ -13,9 +16,60 @@ using namespace std;
 #define SERVER_IP "127.0.0.1"
 #define MAX_MSG_SIZE 2048
 
-int main() {
+string serviceName_reg = "brokerageCompliance";
+string serverAddress_reg = "ServiceServer.elec477grp2";
+static atomic<uint32_t> serialNumber_reg = 1;
+
+int sendRequest() {
     // Initialize protobuf
     GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    if (!svcDir::setSeverAddress(serverAddress_reg)) {
+        cerr << "Failed to set server address!" << endl;
+        exit(1);
+    }
+    else { cout << "Server Address set." << endl;}
+
+    svcDir::serverEntity entity = svcDir::searchService(serviceName_reg);
+    if (entity.name == "init" || entity.port == 0) {
+        std::cerr << "Error: No entity found for service " << serviceName_reg << std::endl;
+        exit(1); // Exit program if service was not found
+    }
+    std::string serverName = entity.name;
+    unsigned short regClientPort = entity.port;
+    std::cout << "Found service: " << serverName << " on port " << regClientPort << std::endl;
+
+
+    // Resolve the IP address from the service name
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;   // IPv4
+    hints.ai_socktype = SOCK_DGRAM; // UDP
+    hints.ai_protocol = IPPROTO_UDP;
+
+    int status = getaddrinfo(serverName.c_str(), nullptr, &hints, &res);
+    if (status != 0) {
+        std::cerr << "Failed to resolve server address: " << gai_strerror(status) << std::endl;
+        return 1;
+    }
+
+    // Extract IP address
+    struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
+    char ipStr[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &addr->sin_addr, ipStr, sizeof(ipStr));
+
+    std::cout << "Resolved IP Address: " << ipStr << std::endl;
+
+
+    // Configure server address
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(regClientPort);
+    servaddr.sin_addr = addr->sin_addr;
+
+    freeaddrinfo(res); // Free the addrinfo struct
+
 
     // Create UDP socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -24,12 +78,6 @@ int main() {
         return 1;
     }
 
-    // Configure server address
-    struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERVER_PORT);
-    servaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
     // Construct EndOfDaySummaryRequest message
     regulatory_to_broker::BrokerageRuleViolationRequest request;
@@ -65,28 +113,40 @@ int main() {
     cout << "Request sent successfully.\n";
 
     // Receive response
+    struct timeval timeout;
+    timeout.tv_sec = 2;
+    timeout.tv_usec = 0;
+
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        close(sockfd);
+        cerr << "Response timeout failed" << endl;
+        return false;
+    }
+
     char buffer[MAX_MSG_SIZE];
     n = recvfrom(sockfd, buffer, MAX_MSG_SIZE, MSG_WAITALL,
                  (struct sockaddr*)&servaddr, &len);
     if (n < 0) {
         perror("recvfrom failed");
         close(sockfd);
-        return 1;
+        sendRequest();
     }
-
-    // Deserialize response
-    regulatory_to_broker::BrokerageRuleViolationResponse response;
-    if (!response.ParseFromArray(buffer, n)) {
-        cerr << "Failed to parse response.\n";
+    else{
+        regulatory_to_broker::BrokerageRuleViolationResponse response;
+        if (!response.ParseFromArray(buffer, n)) {
+            cerr << "Failed to parse response.\n";
+            close(sockfd);
+            return 1;
+        }
+        cout << "Server Response: " << response.DebugString() << endl;
+        serialNumber_reg.fetch_add(1);
         close(sockfd);
-        return 1;
     }
-
-    cout << "Server Response: " << response.DebugString() << endl;
-
-    // Cleanup
-    close(sockfd);
     google::protobuf::ShutdownProtobufLibrary();
-
+    
     return 0;
+}
+
+int main() {
+    sendRequest();
 }
