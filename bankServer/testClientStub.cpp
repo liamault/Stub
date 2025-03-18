adb906e5769d7cdb126732798545ffaa8e0f7115
SyedMoiz1 <95774468+SyedMoiz1@users.noreply.github.com>
2025-03-18 00:14:58 -0400

    banktestclient

--- bankServer/testClientStub.cpp
+++ bankServer/testClientStub.cpp
@@ -6,6 +6,8 @@
 #include "bank_to_brokerage.pb.h"
 #include "common.pb.h"
 #include <google/protobuf/descriptor.h>
+#include "../naming/svcDirClient.hpp"
+
 
 using namespace std;
 
@@ -13,9 +15,63 @@ using namespace std;
 #define SERVER_IP "127.0.0.1"
 #define MAX_MSG_SIZE 2048
 
-int main() {
+
+string serviceName_bank = "BankLedger";
+string serverAddress_bankc = "ServiceServer.elec477grp2";
+static atomic<uint32_t> serialNumber_bankC = 1;
+
+
+int sendDepositRequest(){
     // Initialize protobuf
     GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+    if (!svcDir::setSeverAddress(serverAddress_bankc)) {
+        cerr << "Failed to set server address!" << endl;
+        exit(1);
+    }
+    else { cout << "Server Address set." << endl;}
+
+
+    svcDir::serverEntity entity = svcDir::searchService(serviceName_bank);
+    if (entity.name == "init" || entity.port == 0) {
+        std::cerr << "Error: No entity found for service " << serviceName_bank << std::endl;
+        exit(1); // Exit program if service was not found
+    }
+    std::string serverName = entity.name;
+    unsigned short bankClientPort = entity.port;
+    std::cout << "Found service: " << serverName << " on port " << bankClientPort << std::endl;
+
+
+    // Resolve the IP address from the service name
+    struct addrinfo hints, *res;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;   // IPv4
+    hints.ai_socktype = SOCK_DGRAM; // UDP
+    hints.ai_protocol = IPPROTO_UDP;
+
+    int status = getaddrinfo(serverName.c_str(), nullptr, &hints, &res);
+    if (status != 0) {
+        std::cerr << "Failed to resolve server address: " << gai_strerror(status) << std::endl;
+        return 1;
+    }
+
+    // Extract IP address
+    struct sockaddr_in *addr = (struct sockaddr_in *)res->ai_addr;
+    char ipStr[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &addr->sin_addr, ipStr, sizeof(ipStr));
+
+    std::cout << "Resolved IP Address: " << ipStr << std::endl;
+
+
+    // Configure server address
+    struct sockaddr_in servaddr;
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(bankClientPort);
+    servaddr.sin_addr = addr->sin_addr;
+
+    freeaddrinfo(res); // Free the addrinfo struct
+
 
     // Create UDP socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -24,18 +80,12 @@ int main() {
         return 1;
     }
 
-    // Configure server address
-    struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERVER_PORT);
-    servaddr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
     // Construct EndOfDaySummaryRequest message
     bank_to_brokerage::EndOfDaySummaryRequest request;
     request.mutable_header()->set_version(1);
     request.mutable_header()->set_magic(BANK);
-    request.mutable_header()->set_serial(1);
+    request.mutable_header()->set_serial(serialNumber_bankC.load());
 
     auto* transaction1 = request.add_transactions();
     transaction1->set_operation(bank_to_brokerage::EndOfDaySummaryRequest::DEPOSIT);
@@ -43,11 +93,11 @@ int main() {
     transaction1->mutable_amount()->set_cents(2); 
     transaction1->set_component(BROKERAGE);
 
-    auto* transaction2 = request.add_transactions();
-    transaction2->set_operation(bank_to_brokerage::EndOfDaySummaryRequest::WITHDRAW);
-    transaction2->mutable_amount()->set_dollars(3);
-    transaction2->mutable_amount()->set_cents(4); 
-    transaction2->set_component(BROKERAGE);
+    // auto* transaction2 = request.add_transactions();
+    // transaction2->set_operation(bank_to_brokerage::EndOfDaySummaryRequest::WITHDRAW);
+    // transaction2->mutable_amount()->set_dollars(3);
+    // transaction2->mutable_amount()->set_cents(4); 
+    // transaction2->set_component(BROKERAGE);
 
     // Serialize request
     string requestStr;
@@ -70,29 +120,47 @@ int main() {
     cout << "Request sent successfully.\n";
 
     // Receive response
+    struct timeval timeout;
+    timeout.tv_sec = 2;
+    timeout.tv_usec = 0;
+
+    if (setsockopt(sockfd, SOL_SOCKED, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+        close(sockfd);
+        cerr << "Response timeout failed" << endl;
+        return false;
+    }
+
+    // Receive response
     char buffer[MAX_MSG_SIZE];
     n = recvfrom(sockfd, buffer, MAX_MSG_SIZE, MSG_WAITALL,
                  (struct sockaddr*)&servaddr, &len);
     if (n < 0) {
-        perror("recvfrom failed");
+        perror("No response received from bank");
         close(sockfd);
-        return 1;
+        sendDepositRequest();
     }
+    else{
+        // Deserialize response
+        bank_to_brokerage::EndOfDayResponse response;
 
-    // Deserialize response
-    bank_to_brokerage::EndOfDayResponse response;
-    if (!response.ParseFromArray(buffer, n)) {
-        cerr << "Failed to parse response.\n";
+        if (!response.ParseFromArray(buffer, n)) {
+            cerr << "Failed to parse response.\n";
+            close(sockfd);
+            return 1;
+        }
+
+        const std::string statusStr = bank_to_brokerage::EndOfDayResponse::Status_Name(response.status());
+        cout << "Server Response: " << statusStr << endl;
+        serialNumber_bankC.fetch_add(1);
+        // Cleanup
         close(sockfd);
-        return 1;
     }
-
-    const std::string statusStr = bank_to_brokerage::EndOfDayResponse::Status_Name(response.status());
-    cout << "Server Response: " << statusStr << endl;
-
-    // Cleanup
-    close(sockfd);
     google::protobuf::ShutdownProtobufLibrary();
 
     return 0;
+}
+
+int main(){
+    sendDepositRequest();
+
 }
